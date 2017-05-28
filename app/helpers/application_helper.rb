4a4930943a7008e7c34bfe0939e4d94ec3888e5f
Laurie Gloge <laurie@friendlyroad.com>
2017-05-28 18:24:47 +0000

    fixed css, exept slider needs to be responsive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "RockyMountainFloppers"
+    meta = "Nostalgia Funny Cars"
+    if page_title.empty?
+      base_title + ":" + meta
+    else
+      page_title + " | " + base_title
+    end
+  end
   
+  def active_class(link_path)
+    current_page?(link_path) ? 'active' : ""
+  end
 end
