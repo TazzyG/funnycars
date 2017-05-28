ebfd513266a302553c257a514fcca4bbcb80ecfe
Laurie Gloge <laurie@friendlyroad.com>
2017-05-28 21:58:46 +0000

    change region to us-east-2 it was us-west-2

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
         provider:              'AWS',
         aws_access_key_id:     ENV["S3_ACCESS_KEY_ID"],
         aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-        region: 'us-west-2',
+        region: 'us-east-2',
 
     }
     config.cache_dir = "#{Rails.root}/tmp/uploads"
