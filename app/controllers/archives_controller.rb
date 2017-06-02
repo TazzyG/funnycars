fab9660e3dcbaf14da02f168a805809b1fdd7837
Laurie Gloge <laurie@friendlyroad.com>
2017-06-02 14:57:10 +0000

    completed archive news_feed

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,6 +1,25 @@
 class ArchivesController < ApplicationController
+ 
   
   def photos_2010
     @photos_2010 = NewsFeed.all.order("created_at DESC ")
+  end
+  def photos_2011
+    @photos_2011 = NewsFeed.all.order("created_at DESC ")
+  end
+  def photos_2012
+    @photos_2012 = NewsFeed.all.order("created_at DESC ")
+  end
+  def photos_2013
+    @photos_2013 = NewsFeed.all.order("created_at DESC ")
+  end
+  def photos_2014
+    @photos_2014 = NewsFeed.all.order("created_at DESC ")
+  end
+  def photos_2015
+    @photos_2015 = NewsFeed.all.order("created_at DESC ")
+  end
+  def photos_2016
+    @photos_2016 = NewsFeed.all.order("created_at DESC ")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,12 @@ Rails.application.routes.draw do
   resources :news_feeds
   
   get '/photos2010', to: 'archives#photos_2010'
-  
+  get '/photos2011', to: 'archives#photos_2011'
+  get '/photos2012', to: 'archives#photos_2012'
+  get '/photos2013', to: 'archives#photos_2013'
+  get '/photos2014', to: 'archives#photos_2014'
+  get '/photos2015', to: 'archives#photos_2015'
+  get '/photos2016', to: 'archives#photos_2016'
   
   resources :sponsors
   resources :standings
