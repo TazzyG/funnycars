e6f28f0df53fafb08e48db16b7b6576a62bce1a5
Laurie Gloge <laurie@friendlyroad.com>
2017-06-02 18:08:12 +0000

    added links for delete edit on show and index pages

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,5 +1,5 @@
 class RulesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
   before_action :set_rule, only: [:show, :edit, :update, :destroy]
 
   def index
