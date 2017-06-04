049fb8097a5d9bb9beefe88d88063c8e3edc7374
Laurie Gloge <laurie@friendlyroad.com>
2017-06-04 21:59:50 +0000

    add announcements

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
     @articles = Article.all.order("created_at DESC")
     @sponsors = Sponsor.all
     @news_feeds = NewsFeed.where(:created_at =>Chronic.parse('June 2 at 1pm').. Time.now).order("created_at DESC ")
+    @announcements = Announcement.all.order("created_at DESC")
   end
   
   def nl2br(s)
