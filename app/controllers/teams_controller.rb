0d54accb76dea53c358173dd82b339ab14ebab54
Laurie Gloge <laurie@friendlyroad.com>
2017-06-05 17:46:10 +0000

    Fix missing div in team show page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,7 +3,7 @@ class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
 
   def index
-    @teams = Team.all.includes[:event_teams]
+    @teams = Team.all
     @articles = Article.all
   end
   
